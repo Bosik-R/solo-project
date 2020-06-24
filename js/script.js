8b7dffeabcb95caf63700a1ae8f8d71ed08458c6
Tomasz Rosik <Bosik27@gmail.com>
2020-06-24 20:32:07 +0200

    Add modals, modifie project structure

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 const adminInterface = {
+
   initPages: function(){
     const thisApp = this;
 
-    thisApp.navLinks = document.querySelectorAll('.sidebar-list div');
+    thisApp.navLinks = document.querySelectorAll('.sidebar-list-buttons div');
     thisApp.pages = document.querySelector('#pages').children;
     thisApp.secondOptionTemplate = '#template-links';
     thisApp.secondOptionWrapper = '.wrapper-second-option';
@@ -11,7 +12,6 @@ const adminInterface = {
 
     thisApp.defaultPage = thisApp.pages[0].id;
     let pageMatchingHash = thisApp.defaultPage;
-    
     
     for (let page of thisApp.pages){
       if (page.id == idFromHash){
@@ -43,8 +43,7 @@ const adminInterface = {
     thisApp.contentWrapper = document.querySelector('.content-wrapper');
     
     function toggleMenu(visible) {
-      
-      thisApp.sidebarWrapper.classList.toggle('show', visible);
+      thisApp.sidebarWrapper.classList.toggle('slide', visible);
       thisApp.contentWrapper.classList.toggle('dim', visible);
       
     }
@@ -60,8 +59,79 @@ const adminInterface = {
     });
   },
 
+  initModal: function() {
+    const thisApp = this;
+
+    function closeModal() {
+      document.getElementById('overlay').classList.remove('show')
+    }
+
+    document.querySelectorAll('#overlay .js--close-modal').forEach(function (btn) {
+      btn.addEventListener('click', function(e) {
+        e.preventDefault();
+        closeModal();
+      });
+    });
+
+    document.querySelector('#overlay').addEventListener('click', function(e) {
+      if(e.target === this) {
+        closeModal();
+      };
+    });
+
+    document.addEventListener('keyup', function(e) {
+      if(e.keyCode === 27) {
+        closeModal()
+      }
+    });
+    document.querySelector('#quit-button').addEventListener('click', function(e) {
+      e.preventDefault();
+      
+      thisApp.activatePage(thisApp.defaultPage);
+    });
+
+    document.querySelectorAll('.button-modal').forEach(function (button) {
+      
+      button.addEventListener('click', function(e) {
+        const clickedElement = this;
+        e.preventDefault();
+
+        const id = '#modal-' + clickedElement.id;
+        
+        thisApp.openModal(id);
+      });
+    });
+  },
+
+  openModal: function(modal) {
+    document.querySelectorAll('#overlay > *').forEach(function(modal) {
+      
+      modal.classList.remove('show')
+    });
+    
+    document.querySelector('#overlay').classList.add('show');
+    document.querySelector(modal).classList.add('show');
+  },
+
+  startPage: function (){
+      const thisApp = this;
+
+    document.querySelector('#enter').addEventListener('click', function(){
+
+      const inputLogin = document.querySelector('#page-login');
+
+      thisApp.activatePage(thisApp.pages[1].id);
+
+      const profileName = document.querySelectorAll('#profile-name');
+      
+      for (let profile of profileName){
+
+        profile.innerHTML = inputLogin.value;
+      }
+    });
+  },
+
   activatePage: function(pageId){
-    console.log(pageId);
     const thisApp = this;
 
     thisApp.wrapper = document.querySelector('.' + pageId + '-wrapper');
@@ -76,47 +146,56 @@ const adminInterface = {
 
     for (let page of thisApp.pages){
       page.classList.toggle('active', page.id == pageId);
+    }
 
-      if (page.id === thisApp.pages[0].id) {
+      if (pageId == thisApp.defaultPage){
+        thisApp.startPage();
+      }
+
+      if (pageId == thisApp.pages[1].id) {
         var ctx = document.getElementById('myChart').getContext('2d');
         var chart = new Chart(ctx, {
-    // 1
-    type: 'bar',
-    data: {
-        // 2
-        labels: ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10"],
-        // 3
-        datasets: [{
-            // 4
-            label: "Signups",
-            // 5
-            backgroundColor: '#8DBEC8',
-            borderColor: '#8DBEC8',
-            // 6
-            data: [ 52, 51, 41, 94, 26, 6, 72, 9, 21, 88 ],
-        },
-        {
-            label: "FTD",
-            backgroundColor: '#F29E4E',
-            borderColor: '#F29E4E',
-            data: [ 6, 72, 1, 0, 47, 11, 50, 44, 63, 76 ],
-        },
-        {
-            label: "Earned",
-            backgroundColor: '#71B374',
-            borderColor: '#71B374',
-            data: [ 59, 49, 68, 90, 67, 41, 13, 38, 48, 48 ],
-            // 7
-            hidden: true,
-        }]
-    },
+          // 1
+          type: 'bar',
+          data: {
+              // 2
+              labels: ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10"],
+              // 3
+              datasets: [{
+                  // 4
+                  label: "Signups",
+                  // 5
+                  backgroundColor: '#8DBEC8',
+                  borderColor: '#8DBEC8',
+                  // 6
+                  data: [ 52, 51, 41, 94, 26, 6, 72, 9, 21, 88 ],
+              },
+              {
+                  label: "FTD",
+                  backgroundColor: '#F29E4E',
+                  borderColor: '#F29E4E',
+                  data: [ 6, 72, 1, 0, 47, 11, 50, 44, 63, 76 ],
+              },
+              {
+                  label: "Earned",
+                  backgroundColor: '#71B374',
+                  borderColor: '#71B374',
+                  data: [ 59, 49, 68, 90, 67, 41, 13, 38, 48, 48 ],
+                  // 7
+                  hidden: true,
+              }]
+          },
         });
+
         thisApp.secondWrapper = document.querySelector(thisApp.secondOptionWrapper); 
         const secondTemplate = Handlebars.compile(document.querySelector(thisApp.secondOptionTemplate).innerHTML);
         const html = secondTemplate();
         thisApp.secondWrapper.innerHTML = html;
       } 
-    }
+    
+    thisApp.initModal();
+
+    window.location.hash = '#/' + pageId;
   },
 
   init: function(){
@@ -124,35 +203,6 @@ const adminInterface = {
 
     thisApp.initPages();
   }
+
 }
 adminInterface.init();
-
-
-/* const dateToStr = function(dateObj){
-  return dateObj.toISOString().slice(0, 10);
-};
-
-
-const datePicker = document.querySelectorAll(`input[name="datePicker"]`);
-
-datePicker.value = dateToStr(new Date());
-console.log(datePicker.value);
-datePicker.input.value = datePicker.value;
-
-flatpickr(datePicker, {
-  defaultDate: datePicker.value,
-  
-  altInput: true,
-  altFormat: "F j, Y",
-  dateFormat: "Y-m-d",
-  {
-  wrap: true,
-  }
-  'locale': {
-    'firstDayOfWeek': 1
-  },
-
-  onChange: function(selectedDates, dateStr){
-    datePicker.value = dateStr;
-  },
-}); */
